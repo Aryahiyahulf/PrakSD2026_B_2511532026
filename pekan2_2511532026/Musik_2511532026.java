9340b56f0bce970b0054142ff4c94ee5d12a77fa
Aryahiyahulf <aryahiyahulf@gmail.com>
2026-04-19 17:43:10 +0700

    Add files via upload

---
+++ pekan2_2511532026/Musik_2511532026.java
@@ -0,0 +1,41 @@
+package pekan2_2511532026;
+
+public class Musik_2511532026 {
+    // Atribut
+    private String judul_2511532026;
+    private String penyanyi_2511532026;
+    private int durasi_2511532026;
+
+    // Constructor
+    public Musik_2511532026(String judul, String penyanyi, int durasi) {
+        this.judul_2511532026 = judul;
+        this.penyanyi_2511532026 = penyanyi;
+        this.durasi_2511532026 = durasi;
+    }
+
+    // Getter
+    public String getJudul_2511532026() {
+        return judul_2511532026;
+    }
+
+    public String getPenyanyi_2511532026() {
+        return penyanyi_2511532026;
+    }
+
+    public int getDurasi_2511532026() {
+        return durasi_2511532026;
+    }
+
+    // Setter
+    public void setJudul_2511532026(String judul) {
+        this.judul_2511532026 = judul;
+    }
+
+    public void setPenyanyi_2511532026(String penyanyi) {
+        this.penyanyi_2511532026 = penyanyi;
+    }
+
+    public void setDurasi_2511532026(int durasi) {
+        this.durasi_2511532026 = durasi;
+    }
+}
